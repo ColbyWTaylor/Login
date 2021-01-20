6b12abaeb91d0113edfda9fb8d80b3adbf3707e6
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-20 00:19:03 -0500

    login text

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,11 @@
 function Login() {
     return (    <>
+    <h2>Login</h2>
     <p>Name:</p><input />
     <p>UltraSecret Passphrase:</p><input />
     <br />
-    <button>Login</button>
+    <button>Login, Bucko</button>
+    <h3\>Register Instead?</h3>
     </>)
 }
 
