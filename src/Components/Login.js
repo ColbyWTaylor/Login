b22da78805e0a54ddedc9080f5a4e5f0c1a461c7
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-20 00:15:38 -0500

    login component temp added

---
+++ src/Components/Login.js
@@ -0,0 +1,10 @@
+function Login() {
+    return (    <>
+    <p>Name:</p><input />
+    <p>UltraSecret Passphrase:</p><input />
+    <br />
+    <button>Login</button>
+    </>)
+}
+
+export default Login;
