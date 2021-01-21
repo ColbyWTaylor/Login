6418912e077e7236b60760950d881bd1b0a16228
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-20 21:48:12 -0500

    input name with hook

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,18 @@
+import React, {useState} from 'react'
+
 function Login() {
+
+    const submit = () => {console.log("button clickedd")}
+    const [username, setUsername] = useState("")
+
+
     return (    <>
     <h2>Login</h2>
-    <p>Name:</p><input />
+    <p>Name:</p><input onChange={e => setUsername(e.target.value)} />
+    <p>current name: {username}</p>
     <p>UltraSecret Passphrase:</p><input />
     <br />
-    <button>Login, Bucko</button>
+    <button onClick={submit}>Login, bucko</button>
     <h3>Register Instead?</h3>
     </>)
 }
