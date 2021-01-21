36ac5dc1ecce987c5c8a22c7fb52560d3bb62ef6
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-20 21:52:17 -0500

    hooks for username and password

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,15 +2,16 @@ import React, {useState} from 'react'
 
 function Login() {
 
-    const submit = () => {console.log("button clickedd")}
+    const submit = () => {console.log("user name: " + username + ", password: " + password)}
     const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
 
     return (    <>
     <h2>Login</h2>
     <p>Name:</p><input onChange={e => setUsername(e.target.value)} />
     <p>current name: {username}</p>
-    <p>UltraSecret Passphrase:</p><input />
+    <p>UltraSecret Passphrase:</p><input type="password" onChange={e => setPassword(e.target.value)}/>
     <br />
     <button onClick={submit}>Login, bucko</button>
     <h3>Register Instead?</h3>
