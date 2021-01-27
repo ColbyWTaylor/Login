528a737b9dfa18fdf6afc443472ba5e6116fbed9
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-26 23:22:53 -0500

    form display logic

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,21 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function Login() {
+  const submit = () => {
+    console.log("user name: " + username + ", password: " + password);
+  };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [registeredUser, setRegisteredUser] = useState(false);
 
-    const submit = () => {console.log("user name: " + username + ", password: " + password)}
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-
-
-    return (    <>
-    <h2>Login</h2>
-    <p>Name:</p><input onChange={e => setUsername(e.target.value)} />
-    <p>current name: {username}</p>
-    <p>UltraSecret Passphrase:</p><input type="password" onChange={e => setPassword(e.target.value)}/>
-    <br />
-    <button onClick={submit}>Login, bucko</button>
-    <h3>Register Instead?</h3>
-    </>)
+  return (
+    <>
+      {registeredUser && <h1>Login</h1>}
+      {!registeredUser && <h1>Register</h1>}
+      <p onClick={() => setRegisteredUser(!registeredUser)}>
+        {registeredUser ? "Register instead" : "Login instead"}
+      </p>
+      <p>Name: {username}</p>
+      <input onChange={(e) => setUsername(e.target.value)} />
+      <p>UltraSecret Passphrase:</p>
+      <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <br />
+      <button onClick={submit}>Submit</button>
+    </>
+  );
 }
 
 export default Login;
