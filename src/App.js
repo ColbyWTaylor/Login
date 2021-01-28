e02e781e7f33a2eaa2627e1e62835b152f3b5f6b
ColbyWTaylor <colbyt.093@gmail.com>
2021-01-27 21:57:52 -0500

    react router + basic react router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import './App.css';
-import Login from './Components/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/link2">Link 2</Link>
+            </li>
+            <li>
+              <Link to="/link3">Link 3</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <p>here is home</p>
+          </Route>
+          <Route path="/link2">
+            <p>page 1</p>
+          </Route>
+          <Route path="/link3">
+            <p>page2</p>
+          </Route>
+        </Switch>
 
-      <Login />
-
-
-    </div>
+        <Login />
+      </div>
+    </Router>
   );
 }
 
